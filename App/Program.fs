1c21390156019ec51cdc5c76383d2f23b5a529d7
pokreman <pokreman06@gmail.com>
2024-10-23 15:24:47 -0600

    temp

--- App/Program.fs
+++ App/Program.fs
@@ -1,5 +1,13 @@
 ﻿open System.Collections.Immutable;
-let living = ImmutableSortedSet<int*int>.Empty
-living = living.Add (4,8)
+let startBoard = Set<int*int> []
 
-printfn "%A" living
+let delta = Set.empty.Add(1,1).Add(0,1).Add(-1,1).Add(1,0).Add(0,0).Add(-1,0).Add(1,-1).Add(0,-1).Add(-1,-1)
+
+let addtup ((x1, y1): int*int) ((x2, y2): int*int) = 
+    (x1+x2, y1+y2)
+ 
+let rec deltaTup ((x,y): int*int) ((set: Set<int*int>)) n:int =
+    (deltaTup (x,y) set
+assert((3,4)=(addtup (1,1) (2,3)))
+
+    
